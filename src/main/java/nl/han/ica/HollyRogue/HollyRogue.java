886e0853abe71053fb1cb93609f4a651d27ca5eb
Darremo <timonjohan@gmail.com>
2018-10-25 16:00:52 +0200

    Initial Commit

---
+++ src/main/java/nl/han/ica/HollyRogue/HollyRogue.java
@@ -0,0 +1,71 @@
+package nl.han.ica.HollyRogue;
+
+import javazoom.jl.player.Player;
+import nl.han.ica.HollyRogue.tiles.WallTile;
+import nl.han.ica.oopg.engine.GameEngine;
+
+/**
+ * Created by timon on 23-10-2018.
+ */
+import nl.han.ica.oopg.objects.GameObject;
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.objects.TextObject;
+import nl.han.ica.oopg.tile.TileMap;
+import nl.han.ica.oopg.tile.TileType;
+import nl.han.ica.oopg.view.View;
+
+public class HollyRogue extends GameEngine {
+    private Player player;
+    private TileMap tileMap;
+
+
+    public static String MEDIA_URL = "src/main/java/nl/han/HollyRogue/media/";
+
+    public static void main(String[] args) {
+        HollyRogue hr = new HollyRogue();
+        hr.runSketch();
+    }
+
+    @Override
+    public void setupGame() {
+        int worldWidth = 500;
+        int worldHeight = 500;
+
+        player = new Player(this);
+        addGameObject(player, 200, 200);
+
+        View view = new View(worldWidth, worldHeight);
+
+        setView(view);
+        size(worldWidth, worldHeight);
+    }
+
+    private void initializeTileMap() {
+        //Sprite Loading
+        Sprite wallSprite = new Sprite(this.MEDIA_URL.concat("Sprite.png"));
+
+        TileType<WallTile> wallTileType = new TileType<>(WallTile.class, wallSprite);
+
+        TileType[] tileTypes = {wallTileType};
+        int tileSize = 64;
+        int tilesMap[][] = {
+                {-1, -1, -1, -1, -1, -1, -1, },
+                {-1, -1, -1, -1, -1, -1, -1, },
+                {-1, -1, -1, -1, -1, -1, -1, },
+                {-1, -1, -1, -1, -1, -1, -1, },
+                {0, 0, 0, 0, 0, 0, 0, },
+                {-1, -1, -1, -1, -1, -1, -1, },
+                {-1, -1, -1, -1, -1, -1, -1, },
+        };
+        tileMap = new TileMap(tileSize, tileTypes, tilesMap);
+    }
+
+
+
+    @Override
+    public void update() {
+        // Dit doet nog niets
+
+    }
+
+}
---
+++ src/main/java/nl/han/ica/HollyRogue/tiles/WallTile.java
@@ -0,0 +1,14 @@
+package nl.han.ica.HollyRogue.tiles;
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
+
+public class WallTile extends Tile {
+    /**
+     *
+     * @param sprite image to be drawn as a floor Tile.
+     */
+    public WallTile(Sprite sprite) {
+        super(sprite);
+    }
+}
